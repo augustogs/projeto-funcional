df9aa1de8680e39439f5175a37ab5304ec2d7bb2
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-08-15 21:05:48 -0300

    create orderBy, groupBy functions

---
+++ src/app/utils/util.ts
@@ -0,0 +1,27 @@
+import { Branch } from "../interfaces/branch";
+
+export function orderBy(key: any, data: any[]): any {
+    const dataCopy = [...data]
+
+    const result = dataCopy.sort((a, b) => {
+        if (a[key] > b[key]) return 1;
+        if (a[key] < b[key]) return -1;
+        return 0;
+    });
+    return result;
+}
+
+export function groupBy(key: any, data: Branch[]): any {
+    const dataCopy = [...data]
+    const dataOrdered = orderBy('nome', dataCopy)
+
+    let result = dataOrdered.reduce((partial: any, actual: any) => {
+        const group = actual[key]
+
+        if(!partial[group]) partial[group] =[]
+        partial[group].push(actual)
+        return partial
+    }, {});
+    return result;
+}
+
